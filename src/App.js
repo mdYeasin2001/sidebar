7a9f942a53d3b3a2a484037e9db2da7ce65a6a3b
Ashraful-Mijan <ashrafulmijan5@gmail.com>
2021-06-17 20:56:15 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Sidebar from './Components/Sidebar/Sidebar';
+
+function App() {
+  return (
+    <div>
+      <Sidebar/>
+    </div>
+  );
+}
+
+export default App;
